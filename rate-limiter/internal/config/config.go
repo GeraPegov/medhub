3cdbeb6e7621318edaabdab5756d34c011eaab31
gera <gera51666@gmail.com>
2026-08-24 21:05:43 +0300

    key and env exammple

--- rate-limiter/internal/config/config.go
+++ rate-limiter/internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"errors"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -12,13 +13,16 @@ type MedhubDB struct {
 }
 
 func Load() (*MedhubDB, error) {
-	err := godotenv.Load(".env")
-	if err != nil {
-		return nil, err
-	}
+	_ = godotenv.Load(".env")
 	db := MedhubDB{
 		DB_URL:    os.Getenv("PROD_DB_URL"),
 		SecretKey: os.Getenv("SECRET_KEY"),
+	}
+	if db.DB_URL == "" {
+		return nil, errors.New("PROD_DB_URL is required")
+	}
+	if db.SecretKey == "" {
+		return nil, errors.New("SECRET_KEY is required")
 	}
 	return &db, nil
 }
